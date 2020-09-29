45a3c50e4279f9f8dd9ea7f6f311b1302543ce42
ricardonava <rnavarrete@quaxar.com>
2020-09-29 17:55:46 -0500

    Task 4: Refactoring room details

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -6,7 +6,7 @@ import { addNavigationHelpers, StackNavigator, TabNavigator, NavigationActions }
 
 import ExploreTab from '../components/MainScreen/ExploreTab';
 import ProfileTab from '../components/MainScreen/ProfileTab';
-import DetailScreen from '../components/DetailScreen';
+import RoomScreen from '../components/RoomScreen';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -56,11 +56,11 @@ export const AppNavigator = StackNavigator({
       header: null,
     }
   },
-  Detail: {
-    screen: DetailScreen,
-    navigationOptions: {
-      title: 'Code4Startup',
-    }
+  Room: {
+    screen: RoomScreen,
+    navigationOptions: (props) => ({
+      title: props.navigation.state.params.item.title,
+    })
   },
 });
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -13,3 +13,28 @@ export function normalizeRooms(rooms) {
         }
     })
 }
+
+export function normalizeRoom(room) {  
+        return {
+            id: room.id || '',
+            title: room.listing_name || '',
+            image: `${HOST}${room.image}` || '',
+            homeType: room.home_type || '',
+            bedRoom: room.bed_room || '',
+            price: room.price || '',
+            instant: room.instant || '',
+            summary: room.summary || '',
+            accomodate: room.accomodate || '',
+            bathRoom: room.bath_room || '',
+            unavailableDates: room.unavailable_dates || '',
+            host: room.host ? {
+                email: room.host.email || '',
+                fullname: room.host.fullname || '',
+                avatar: room.host.avatar || '',
+            } : {
+                email: '',
+                fullname: '',
+                avatar: '',
+            }
+        }
+}
