ed3e2a155d8e24e24118544516372c9f1f1fbbe3
ricardonava <rnavarrete@quaxar.com>
2020-09-29 18:15:17 -0500

    Task 4: Room Action and Reducer

--- src/actions/room.js
+++ src/actions/room.js
@@ -1,12 +1,20 @@
 import { HOST } from '../constants';
-import { normalizeRooms } from '../utils';
+import { normalizeRooms, normalizeRoom } from '../utils'; //created at utils/index.js
 
 export const SET_ROOMS = 'SET_ROOMS';
+export const SET_ROOM = 'SET_ROOM';
 
 export function setRooms(rooms){
     return {
         type: SET_ROOMS,
         rooms
+    }
+}
+
+export function setRoom(room){
+    return {
+        type: SET_ROOM,
+        room
     }
 }
 
@@ -19,6 +27,23 @@ export function getRooms(){
 
             if (json.is_success){
                 dispatch(setRooms(normalizeRooms(json.rooms)));
+            }else{
+                alert(json.error);
+            }
+        })
+        .catch(e => alert(e));
+    }
+}
+
+export function getRoom(roomId){
+    return (dispatch) => {
+        return fetch(`${HOST}/api/v1/rooms/${roomId}`)
+        .then(response => response.json())
+        .then(json => {
+            console.log("get a single room", json); //To debug to see what data get from server
+
+            if (json.is_success){
+                dispatch(setRoom(normalizeRoom(json.room)));
             }else{
                 alert(json.error);
             }
--- src/components/RoomScreen/index.js
+++ src/components/RoomScreen/index.js
@@ -7,6 +7,9 @@ import {
   Text,
 } from 'react-native';
 
+import { getRoom } from '../../actions/room';
+import room from '../../reducers/room';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,22 +22,33 @@ const styles = StyleSheet.create({
 });
 
 class RoomScreen extends Component {
+
+  componentWillMount(){
+    const selectedRoomId = this.props.navigation.state.params.item.id;
+    this.props.getRoom(selectedRoomId);
+  }
+
   render() {
+
+    const room = this.props.room;
+
+    if (!room) return null; // return null if the room doesn't exists
+
     const item = this.props.navigation.state.params.item;
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>{item.title}</Text>
+        <Text style={styles.text}>{room.host.fullname}</Text>
       </View>
     );
   }
 }
 
 const mapStateToProps = state => ({
-
+  room: state.room.room,
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  getRoom: (roomId) => dispatch(getRoom(roomId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RoomScreen);
