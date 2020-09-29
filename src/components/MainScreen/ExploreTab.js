637d708a8dbc6144dd859e5241bc67b1360bd1b7
ricardonava <rnavarrete@quaxar.com>
2020-09-28 21:57:31 -0500

    Task 3: Room Redux

--- src/components/MainScreen/ExploreTab.js
+++ src/components/MainScreen/ExploreTab.js
@@ -35,54 +35,18 @@ const styles = StyleSheet.create({
   }
 });
 
-const items = [
-  {
-    id: 1,
-    title: 'New York',
-    homeType: 'House',
-    image: {uri: 'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/120445102/original/582239b31d9888936cad4611b98dd470dd50d2e3/build-you-a-nice-house-in-blocksburg.jpeg'},
-    bedRoom: 2,
-    price: 150,
-    instant: true
-  },
-  {
-    id: 2,
-    title: 'Coatza',
-    homeType: 'House',
-    image: {uri: 'https://i.pinimg.com/originals/77/c4/d5/77c4d536db08d773f1aaa3d634425a14.jpg'},
-    bedRoom: 2,
-    price: 150,
-    instant: false
-  },
-  {
-    id: 3,
-    title: 'Minatitlan',
-    homeType: 'House',
-    image: {uri: 'https://odis.homeaway.com/odis/listing/68c67a92-f398-4463-ba1a-5d82fd541193.c10.jpg'},
-    bedRoom: 2,
-    price: 150,
-    instant: true
-  },
-];
-
 class ExploreTab extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: items
-    };
-  }
 
   onPress(item) {
     this.props.navigate({ routeName: "Detail", params: { item: item } });
   }
 
   render() {
+    const { rooms } = this.props;
     return (
       <FlatList
         style={styles.container}
-        data={this.state.items}
+        data = { rooms }
         renderItem={({item}) => 
           <TouchableOpacity onPress={() => this.onPress(item)} style={styles.item}>
             <Image style = {styles.image} source = {item.image} />
@@ -97,7 +61,7 @@ class ExploreTab extends Component {
 }
 
 const mapStateToProps = state => ({
-
+  rooms: state.room.rooms
 });
 
 const mapDispatchToProps = dispatch => ({
